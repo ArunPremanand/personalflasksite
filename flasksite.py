2bb633fef531c42a1f396d48d57f0df0e90ce3ec
Arun Premanand <arun.prem5@gmail.com>
2023-11-18 17:21:47 -0800

    commit

--- flasksite.py
+++ flasksite.py
@@ -10,5 +10,9 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route("/gallery")
+def gallery():
+    return render_template('gallery.html')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=49750)
